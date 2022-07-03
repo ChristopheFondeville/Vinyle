ae1aadcfb9260ad0849a6934855c92d4a553b863
Christophe Fondeville <fondevillechristophe@gmail.com>
2022-07-03 23:53:10 +0200

    Formulaire connexion

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
@@ -22,5 +23,10 @@ class LoginController extends AbstractController
             'last_username' => $lastUsername,
             'error'         => $error,
         ]);
+    }
+
+    #[Route(path: 'logout', name: 'app_logout')]
+    public function logout(): void
+    {
     }
 }
