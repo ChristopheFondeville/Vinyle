2b91432a972886d49baae241cb65968c8bc5452d
Christophe Fondeville <fondevillechristophe@gmail.com>
2022-07-21 10:16:35 +0200

    Ajout formulaire Add Vinyle

--- src/Controller/AlbumController.php
+++ src/Controller/AlbumController.php
@@ -2,17 +2,40 @@
 
 namespace App\Controller;
 
+use App\Entity\Album;
+use App\Form\AddAlbumType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AlbumController extends AbstractController
 {
     #[Route('/album/add', name: 'app_album_add')]
-    public function index(): Response
+    public function addAlbum(Request $request): Response
     {
-        return $this->render('album/add_album.html.twig', [
-            'controller_name' => 'AlbumController',
+        $album = new Album();
+        $formAddAlbum = $this->createForm(AddAlbumType::class, $album);
+        $formAddAlbum->handleRequest($request);
+
+        return $this->renderForm('album/add_album.html.twig', [
+            'formAddAlbum' => $formAddAlbum,
+        ]);
+    }
+
+    #[Route('/album/edit', name: 'app_album_edit')]
+    public function editAlbum(): Response
+    {
+        return $this->render('album/edit_album.html.twig', [
+            'controller_name' => 'Modifier album',
+        ]);
+    }
+
+    #[Route('/album/delete', name: 'app_album_delete')]
+    public function deleteAlbum(): Response
+    {
+        return $this->render('album/delete_album.html.twig', [
+            'controller_name' => 'Supprimer album',
         ]);
     }
 }
